fb0615e6972063a83876ce245678ab0054ef98fc
Artem Bebenin <artjombebenin@gmail.com>
2019-04-28 16:18:19 +0300

    arrow_functions_final

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('hi!')
+
+//+++
+console.log('+++ short notations for arror functions')
+
+let arrowFunction1 = () => 'simple text from arrow function 1'
+
+let arrowFunction2 = (a, b) => ('AF2,  a + b = ' + (a + b))
+
+let arrowFunction3 = (num) => {
+  if (num > 0) { 
+    return  num + ' is positive number!' 
+  }
+}
+
+console.log(arrowFunction1())
+console.log(arrowFunction2(1, 2))
+console.log(arrowFunction3(5))
+
+//+++
+console.log('+++ context this')
+let simpleObj = {
+  variable: 'variable',
+  method: function () {
+    let arowFunction = () => {
+      console.log(this.variable)
+    }
+    arowFunction()
+  }
+}
+
+simpleObj.method() // variable
+
+
+
+
