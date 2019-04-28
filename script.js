0be55dc24c31de87b2d79664e71c1e1b0163d391
Artem Bebenin <artjombebenin@gmail.com>
2019-04-28 15:14:30 +0300

    const let variables final

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 
-console.log('-let block scope ----')
-var i = 10
+//++++
+console.log('-block scope ----')
 {
-  // i = 4 - error with "Uncaught ReferenceError: i is not defined"
-  let i = 0
-  console.log('i form block: ', i)
+  var variableVar = 'variable var'
+  let variableLet = 'variable let'
 }
-console.log('i form gloabal: ', i)
+console.log(variableVar)
+// console.log(variableLet) // - ReferenceError: variableLet is not defined
 
+//++++
+console.log('-nested block scope---')
+{
+  let block_1 = 'block_1'
+  {
+    // console.log(block_1) // ReferenceError: block_1 is not defined
+  }
+  function show_block_1() { console.log(block_1) }
+  show_block_1()
+}
+
+// +++ 
+console.log('- initialization let variables ------')
+console.log(varA) // undefined
+var varA = 'this is variable A'
+// console.log(varB) //  ReferenceError: varB is not defined
+let varB = 'this is variable B'
+
+//++++++++
 console.log('-const redifenetions-----------------')
-
-// const ARR - error Uncaught SyntaxError: Missing initializer in const declaration
+// const ARR // - error Uncaught SyntaxError: Missing initializer in const declaration
 const ARR = [2]
-// ARR = { 2: 2 } - Uncaught TypeError: Assignment to constant variable.
+// ARR = [2] // - Uncaught TypeError: Assignment to constant variable.
 ARR.push(3)
-{
-  const ARR = 'ARR in block!'
-  console.log(ARR)
-}
-console.log('const ARR in global', ARR) 
-
+console.log('const ARR', ARR) // [2,3]
