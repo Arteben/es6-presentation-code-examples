b836af81c1c7270feb88360ab0a5de71c824c82a
Artem Bebenin <artjombebenin@gmail.com>
2019-05-04 19:38:14 +0300

    default parameters add logs

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ var sum = function (a = 1, b = 1) {
   return a + b
 }
 
-console.log('default sum (1 + 1)', sum())
-console.log('sum  10 + 10, ', sum(10, 10))
+console.log('default sum (1 + 1) = ', sum())
+//default sum (1 + 1) =  2
+
+console.log('sum  10 + 10 =  ', sum(10, 10))
+//sum  10 + 10 =   20
