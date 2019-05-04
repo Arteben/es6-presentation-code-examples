22bc8346f200b3b68f371fd6359375b10c360119
Artem Bebenin <artjombebenin@gmail.com>
2019-05-04 18:44:01 +0300

    promises final

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('hi!')
+
+// ++++ simple promises
+console.log('simple promises----------')
+var simplePromise = new Promise((_resolve, _reject) => {
+  _resolve('begin')
+})
+
+simplePromise.then((str) => `${str} thirst`)
+             .then((str) => `${str} second`)
+             .then((str) => console.log(str))
+
+// ++ promises with catch
+console.log('--- catch promises-----------')
+var catchPromise = new Promise ((_resolve, _reject) => {
+ console.log('Some error!')
+ _reject()
+})
+
+catchPromise.then(() => { }).then(()=>{ }).then()
+catchPromise.catch(() => {
+ console.log('catch error!')
+})
