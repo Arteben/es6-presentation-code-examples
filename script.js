a8eb62df058d635c3f5a33ad9b792a69e3a0a577
Artem Bebenin <artjombebenin@gmail.com>
2019-05-04 19:39:17 +0300

    spead / rest add logs

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let arr = [1,2,3]
 console.log('arr elements ', ...arr)
+// arr elements  1 2 3
 
 let getArgs = (...args) => console.log(args)
 getArgs(1, 'arg2', [3])
+// Array(3) [1, "arg2", Array(1)]
